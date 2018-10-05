f3b69ba2ff297b04116f0b059b9bb239d4b7726a
Nyyani <43067342+Nyyani@users.noreply.github.com>
2018-10-05 15:16:09 +0100

    camera follows player

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 
+    public Transform followTarget;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void LateUpdate () {
+
+        if (followTarget != null)
+            {
+            Vector3 newPosition = followTarget.position;
+            newPosition.z = transform.position.z;
+            transform.position = newPosition;
+        }
 	}
 }
