0693d0270e6b70889d816488023e614b44f65e4d
Nyyani <43067342+Nyyani@users.noreply.github.com>
2018-10-05 14:38:09 +0100

    added level and items

---
+++ Assets/Scripts/ScorePickup.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScorePickup : MonoBehaviour {
+
+    public Score scoreTracker;
+    public int scoreValue = 1;
+
+
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+      //Only do stuff if it's the player that we have hit  
+        if(collision.GetComponent <Player>())
+        {
+            //Updating the score based on how much this item is worth
+            scoreTracker.ChangeValue(scoreValue);
+            //Getting rid of the item object
+            Destroy(gameObject);
+        }
+
+    }
+
+
+
+
+}
